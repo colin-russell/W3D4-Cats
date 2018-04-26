4583958448ab315a46f19090808d0d6adb84d76c
Colin Russell <colinrussell101@gmail.com>
2018-04-26 16:44:46 -0400

    Initial Commit

---
+++ W3D4 Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W3D4 Cats
+//
+//  Created by Colin on 2018-04-26.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
