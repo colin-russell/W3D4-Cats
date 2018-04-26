86ad49ec4ae08ed8e8c8a7fa80ba33de7da80a48
Colin Russell <colinrussell101@gmail.com>
2018-04-26 18:21:17 -0400

    most of the data handling done. starting to create the Photo class object

---
+++ W3D4 Cats/Photo.h
@@ -0,0 +1,13 @@
+//
+//  Photo.h
+//  W3D4 Cats
+//
+//  Created by Colin on 2018-04-26.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Photo : NSObject
+@property (nonatomic, strong)NSDictionary *photoJSON;
+@end
--- W3D4 Cats/ViewController.m
+++ W3D4 Cats/ViewController.m
@@ -16,14 +16,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSURL *flickrCatUrl = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=178d9e3064aa836a8bac2c18eea8957e&tags=cat"];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:flickrCatUrl];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSError *jsonError = nil;
+        NSDictionary *flickrDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        NSDictionary *photoDict = flickrDict[@"photos"][@"photo"];
+     
+        if (jsonError) {
+            NSLog(@"jsonError: %@", jsonError.localizedDescription);
+            return;
+        }
+//        for(NSString *key in [photoDict2 allKeys]) {
+//            NSLog(@"%@",[photoDict2 objectForKey:key]);
+       // }
+        for (NSDictionary *dict in photoDict) {
+            NSString *photoName = dict[@"title"];
+            NSLog(@"image name: %@", photoName);
+            
+        }
+        
+        
+    }];
+    
+    [dataTask resume];
+    
+
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)constructURL {
+    
 }
+
 
 
 @end
