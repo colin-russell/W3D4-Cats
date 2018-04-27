290f3bd44c132fd67edc0fa9e7b6d9a9c6bf4bc4
Colin Russell <colinrussell101@gmail.com>
2018-04-26 20:42:36 -0400

    added setup of collection view

--- W3D4 Cats/ViewController.m
+++ W3D4 Cats/ViewController.m
@@ -9,9 +9,9 @@
 #import "ViewController.h"
 #import "Photo.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource>
 @property (nonatomic, strong) NSMutableArray *photoObjects;
-
+@property (weak, nonatomic) IBOutlet UICollectionView *catCollectionView;
 @end
 
 @implementation ViewController
@@ -19,7 +19,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.photoObjects = [NSMutableArray new];
+    [self setupCollectionView];
+    [self flickrNSURLsetup];
+
+}
+
+- (void)setupCollectionView {
+    self.catCollectionView.dataSource = self;
+    UICollectionViewFlowLayout *flowLayout = [UICollectionViewFlowLayout new];
+    flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    flowLayout.itemSize = CGSizeMake(150, 100);
+    flowLayout.minimumInteritemSpacing = 5;
+    flowLayout.minimumLineSpacing = 5;
+    flowLayout.sectionInset = UIEdgeInsetsMake(10, 20, 5, 20);
+    flowLayout.headerReferenceSize = CGSizeMake(0, 22);
     
+    [self.catCollectionView setCollectionViewLayout:flowLayout];
+}
+
+#pragma mark -NSURL GET
+
+- (void)flickrNSURLsetup {
     NSURL *flickrCatUrl = [NSURL URLWithString:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=178d9e3064aa836a8bac2c18eea8957e&tags=cat"];
     NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:flickrCatUrl];
     
@@ -36,7 +56,7 @@
         NSError *jsonError = nil;
         NSDictionary *flickrDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
         NSDictionary *photoDict = flickrDict[@"photos"][@"photo"];
-     
+        
         if (jsonError) {
             NSLog(@"jsonError: %@", jsonError.localizedDescription);
             return;
@@ -52,10 +72,18 @@
     }];
     
     [dataTask resume];
-    
-
 }
 
 
+#pragma mark - UICollectionViewDataSource
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [UICollectionViewCell new];
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.photoObjects.count;
+}
 
 @end
